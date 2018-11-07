ef47980531572f0afbdc5625e2e35b99a40a497d
Francisco Penovi <francisco.penovi@valtech.com>
2018-11-07 01:18:19 -0300

    Configured enviroments for android and schemes for iOS

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { Something } from 'components/Something';
+import env from 'react-native-config';
 
 export default class App extends Component {
+  componentDidMount() {
+    console.warn(`THIS IS ${env.NAME} ENVIRONMENT`);
+  }
+
   render() {
     return (
       <ThemeProvider theme={{}}>
