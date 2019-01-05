1d6c0bfd9422cdb15e59deab1dc8144bd69f9bb1
Francisco Penovi <francisco.penovi@valtech.com>
2019-01-05 14:52:02 -0300

    Created configuration tree data structure in redux

--- src/reducers/config.js
+++ src/reducers/config.js
@@ -1,14 +1,46 @@
 import { combineReducers } from 'redux';
 import { handleActions } from 'redux-actions';
+import attributes from 'components/HeaderFilter/attributeOrder';
+import attributeMappings from 'components/HeaderFilter/attributeMappings';
+
+const [ASC, DESC] = [1, -1];
 
 const rsiBoundsInitialState = {
-  lower: 30,
-  upper: 70
+  availableRange: [0, 100],
+  selected: [30, 70]
 };
 
-const langInitialState = 'en';
+const langInitialState = {
+  options: [{ label: 'English', value: 'en' }],
+  selected: 0
+};
+
+const stockOrderingInitialState = {
+  options: Object.keys(attributes)
+    .sort((a, b) => attributes[a] - attributes[b])
+    .map(k => ({ label: attributeMappings[k], value: k })),
+  selected: 0
+};
+
+const stockOrderingModeInitialState = {
+  options: [
+    { label: 'Ascending', value: ASC },
+    { label: 'Descending', value: DESC }
+  ],
+  selected: 0
+};
 
 const rsiBounds = handleActions({}, rsiBoundsInitialState);
 const lang = handleActions({}, langInitialState);
+const defaultStockOrdering = handleActions({}, stockOrderingInitialState);
+const defaultStockOrderingMode = handleActions(
+  {},
+  stockOrderingModeInitialState
+);
 
-export default combineReducers({ rsiBounds, lang });
+export default combineReducers({
+  rsiBounds,
+  lang,
+  defaultStockOrdering,
+  defaultStockOrderingMode
+});
